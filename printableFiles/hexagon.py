46c11e26bde2f68b5a8fd3ef748b00743de6f8ef
Peter Esser <me@peter-esser.de>
2024-05-02 11:41:56 +0200

    add magnet holes on sides

--- printableFiles/hexagon.py
+++ printableFiles/hexagon.py
@@ -33,7 +33,9 @@ def generate_hexagon(radius:float, center:_Tuple[float, float]|None=None):
     hexagon = polygon(points=vertices)
     if center is not None:
         hexagon = hexagon.translateX(center[0]).translateY(center[1])
-    return hexagon
+        for i in range(vertices.__len__()):
+            vertices[i] = (vertices[i][0] + center[0], vertices[i][1] + center[1])
+    return hexagon, vertices
 
 # furthest points of hexagon
 hexagonSize = 5.1961525
@@ -52,16 +54,17 @@ def convertOuterHexagonSizeToInnerSize(outerHexagonSize: float):
 
 innerHexagonSize = convertOuterHexagonSizeToInnerSize(hexagonSize)
 
-h = generate_hexagon(hexagonSize)
-hex_org = generate_hexagon(hexagonSize)
+h = generate_hexagon(hexagonSize)[0]
+outerHexagonVertecies:list[list[_Tuple[float, float]]] = []
 
 for i in range(6):
     angle_rad = math.radians(60 * i + 30)  # 60 degrees for each vertex
     x = hexagonSize * math.cos(angle_rad)
     y = hexagonSize * math.sin(angle_rad)
     magnitude = 1.0/math.sqrt(x**2 + y**2)
-    
-    h = h + generate_hexagon(hexagonSize, center=(magnitude*x*innerHexagonSize*2,magnitude*y*innerHexagonSize*2))
+    hexa, vert = generate_hexagon(hexagonSize, center=(magnitude*x*innerHexagonSize*2,magnitude*y*innerHexagonSize*2))
+    outerHexagonVertecies.append(vert)
+    h = h + hexa
 
 
 def shift_line_3d(line, x, y, z):
@@ -74,7 +77,6 @@ def shift_line_3d(line, x, y, z):
     magnitude = np.linalg.norm(direction_vector)
     
     if magnitude == 0:
-        print("magnitude was 0")
         return line  # Handle zero-length line case
     
     # Normalize the direction vector to get a unit vector
@@ -83,14 +85,11 @@ def shift_line_3d(line, x, y, z):
     # Calculate two perpendicular vectors
     if unit_direction[0] == 0 and unit_direction[1] == 0:
         perpendicular_vector1 = np.array([1, 0, 0])
-        print("a")
     else:
         perpendicular_vector1 = np.array([-unit_direction[1], unit_direction[0], 0])
     perpendicular_vector1 /= np.linalg.norm(perpendicular_vector1)
     
     perpendicular_vector2 = np.cross(unit_direction, perpendicular_vector1)
-    print("perpendicular_vector2")
-    print(perpendicular_vector2)
     
     # Shift endpoints along the perpendicular vectors
     translated_p1 = p1 + x * perpendicular_vector1 + y * perpendicular_vector2 + z * unit_direction
@@ -101,16 +100,11 @@ def shift_line_3d(line, x, y, z):
     
     return shifted_line
 
-
-
 def addBevel(object, line: _Tuple[_Tuple[float, float, float], _Tuple[float, float, float]], depth: float):
     # Example usage:
     shifted_line_1 = shift_line_3d(line, depth*0.75, 0, 0)
     shifted_line_2 = shift_line_3d(line, -depth*0.75, 0, 0)
     shifted_line_3 = shift_line_3d(line, 0, -depth, 0)
-    
-    print(line)
-    print(shifted_line_3)
     
     # first, construct a triangle on that line
     triangle = polyhedron(
@@ -136,7 +130,48 @@ h = h.linear_extrude(height=1)
 
 h = addBevel(h, ((0,0,1.2),(5,0,1.2)), 0.3)
 
-h.save_as_scad()
-h.save_as_stl()
+def angle_with_x_axis(lineForHole: _Tuple[_Tuple[float, float], _Tuple[float, float]]):
+    point1, point2 = lineForHole
+    x1, y1 = point1
+    x2, y2 = point2
+    
+    # Calculate direction vector
+    dx = x2 - x1
+    dy = y2 - y1
+    
+    # Calculate angle with x-axis using atan2
+    angle_rad = math.atan2(dy, dx)
+    
+    # Convert angle from radians to degrees (optional)
+    angle_deg = math.degrees(angle_rad)
+    return angle_rad, angle_deg
 
+
+def addMagnetHole(object, magnetHoleHeightAboveGround:float, magnetHoleRadius: float,magnetHoleDepth: float, lineForHole: _Tuple[_Tuple[float, float], _Tuple[float, float]]):
+    # construct high res cylinder for boolean operation
+    
+    cylinderTool = cylinder(h=magnetHoleDepth*2, center=True, r=magnetHoleRadius)
+    
+    centerOfLine = (lineForHole[1][0] - 0.5 * (lineForHole[1][0] - lineForHole[0][0]),lineForHole[1][1] - 0.5 * (lineForHole[1][1] - lineForHole[0][1]))
+    
+    # rotate tool along line
+    _, angle_deg = angle_with_x_axis(lineForHole)
+    cylinderTool = cylinderTool.rotateX(90).rotateZ(angle_deg).translateX(centerOfLine[0]).translateY(centerOfLine[1]).translateZ(magnetHoleHeightAboveGround + magnetHoleRadius)
+    
+    return object - cylinderTool
+
+print("Adding holes")
+
+for hexVerts in outerHexagonVertecies:
+    length = hexVerts.__len__()
+    for i in range(length):
+        p1 = hexVerts[i]
+        p2 = hexVerts[(i+1)%length]
+        h = addMagnetHole(h, 0.25, 0.55/2.0, 0.11, [p1,p2])
+        
+print("finished descibing model")
+h.save_as_scad()
+print("finished save_as_scad")
+# h.save_as_stl()
 print("Ran")
+
